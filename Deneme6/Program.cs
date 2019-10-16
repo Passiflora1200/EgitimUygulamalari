3f0e781e2d7ba014b4fd94a3cb6c064fbe5040ab
Görkem Karaaslan <gorkemkaraaslan91@gmail.com>
2019-10-16 15:47:35 +0300

    yeni eklemeler

---
+++ Deneme6/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Deneme6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Kaçıncı ayda olduğunuzu girin:");
+            int ay = Convert.ToInt32(Console.ReadLine());
+            switch (ay)
+            {
+                
+                case 1:
+                    Console.Write("Ocak");
+                    break;
+                case 2:
+                    Console.Write("Şubat");
+                    break;
+                case 3:
+                    Console.Write("Mart");
+                    break;
+                case 4:
+                    Console.Write("Nisan");
+                    break;
+                case 5:
+                    Console.Write("Mayıs");
+                    break;
+                case 6:
+                    Console.Write("Haziran");
+                    break;
+                case 7:
+                    Console.Write("Temmuz");
+                    break;
+                case 8:
+                    Console.Write("Ağustos");
+                    break;
+                case 9:
+                    Console.Write("Eylül <3");
+                    break;
+                case 10:
+                    Console.Write("Ekim");
+                    break;
+                case 11:
+                    Console.Write("Kasım");
+                    break;
+                case 12:
+                    Console.Write("ARALIK");
+                    break;
+                default:
+                    Console.Write("Ay bilgisini yanlış girdiniz.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Deneme7/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Deneme7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sayilar = { 60, 80, 55, 42, 13, 24, 26, 25, 26, 55, 45, 50, 50, 40 };
+            int sayi, kontrol = 0;
+            Console.Write("Bir sayı giriniz :");
+            sayi = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                if (sayi == sayilar[i])
+                {
+                    kontrol++;
+                }
+            }
+            if (kontrol == 0)
+            {
+                Console.WriteLine("Sayı dizi içinde bulunamadı...");
+            }
+            else
+            {
+                Console.WriteLine("Bu sayı dizide {0} adet var", kontrol);
+            }
+            Console.ReadLine();
+        }
+    }
+}6

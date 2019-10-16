999f302ffbc2b5677b14558d7124db3f82db39ea
Görkem Karaaslan <gorkemkaraaslan91@gmail.com>
2019-10-16 16:24:21 +0300

    Fibonacci Dizisi

--- Deneme7/Program.cs
+++ Deneme7/Program.cs
@@ -28,4 +28,4 @@ namespace Deneme7
             Console.ReadLine();
         }
     }
-}6
+}
---
+++ Deneme8/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Deneme8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 1, z;
+            int sayi;
+
+            Console.WriteLine("Adım sayısını giriniz:");
+            sayi = Convert.ToInt32(Console.ReadLine());         
+
+            Console.WriteLine(x + " " + "\n" + y + " ");
+
+            for (int i = 0; i < sayi - 2; i++)
+            {
+                z = x + y;
+                Console.WriteLine(z);
+                x = y;
+                y = z;
+
+            }
+        }
+    }
+}
